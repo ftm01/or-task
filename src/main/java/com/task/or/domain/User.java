763e23159ee1cf0dc8810ac0bc23699f4db35b5a
aruli <arianruli@gmail.com>
2021-04-05 15:36:37 +0200

    Added User entity and UserDTO

---
+++ src/main/java/com/task/or/domain/User.java
@@ -0,0 +1,40 @@
+package com.task.or.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", unique = true)
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/task/or/web/v1/dtos/user/UserDTO.java
@@ -0,0 +1,19 @@
+package com.task.or.web.v1.dtos.user;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String gender;
+}
